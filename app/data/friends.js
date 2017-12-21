498a95903ca73aad031aea647cdec37dc42071a3
Leo Galey <leogaley@gmail.com>
2017-12-20 21:18:52 -0600

    add main logic and structure

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,12 @@
-var friends = [];
+var friends = [
+	{ name: 'Number 1 All 1s',
+  		photo: 'https://cdn.pixabay.com/photo/2015/04/04/19/13/one-706897_960_720.jpg',
+  		scores: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ] 
+  	},
+  	{ name: 'Number 2 All 5s',
+  		photo: 'https://cdn.pixabay.com/photo/2015/04/04/19/13/one-706897_960_720.jpg',
+  		scores: [ '5', '5', '5', '5', '5', '5', '5', '5', '5', '5' ] 
+  	}
+	];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,6 +4,8 @@ var friends = require("../data/friends.js");
 
 var router = express.Router();
 
+var difference = function (a, b) { return Math.abs(a - b); }
+
 //Middle ware that is specific to this router
 router.use(function timeLog(request, response, next) {
   console.log('Time: ', Date.now());
@@ -21,8 +23,32 @@ router
   //response.sendFile(path.join(__dirname,"../public","survey.html"));
   		console.log(request.body);
   		friends.push(request.body);
+
+      var nerdCount = friends.length;
+      var matchId;
+      var minDifference = 100;
+      for(i=0;i<nerdCount-1;i++){
+         
+         
+         var currentDifference = 0;
+         var questionCount = friends[0].scores.length;
+         for (y=0;y<questionCount;y++){
+            var clientScores = request.body.scores;
+            var fieldScores = friends[i].scores; 
+            currentDifference += difference(clientScores[y],fieldScores[y]);
+         }
+         if (currentDifference < minDifference){
+           matchId = i;
+           minDifference  = currentDifference ;
+         }
+
+
+      }
+
+      response.send(friends[matchId]);
   		//  /app/public/app/data/friends 
 	});
 //\app\routing\app\public\survey.html'
 //app\routing\survey.html
 module.exports = router;
+
